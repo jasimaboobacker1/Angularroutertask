4141bb7476cf56f6dde26feb2d85ca8bc356125c
unknown <jasimaboobacker1>
2024-03-20 12:37:31 +0530

    message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import { CompanyComponent } from './company/company.component';
+import { CareerComponent } from './career/career.component';
+import { BlogComponent } from './blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'Services',
+        component:ServicesComponent
+    },
+    {
+        path:'company',
+        component:CompanyComponent
+    },
+    {
+        path:'career',
+        component:CareerComponent
+    }, {
+        path:'blog',
+        component:BlogComponent
+    },
+
+];
